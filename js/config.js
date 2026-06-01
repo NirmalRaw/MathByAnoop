5be1b9cbfcdceca4feb91b163046432c82b6f361
NIRMAL RATHAUR <nirmal@Mac-2.lan>
2026-06-01 14:43:59 +0530

    Fix WhatsApp number by prepending country code

--- js/config.js
+++ js/config.js
@@ -8,6 +8,6 @@ const API_URL = "https://script.google.com/macros/s/REPLACE_WITH_YOUR_GAS_DEPLOY
 const BRAND = {
   name: "Math By Anoop",
   tagline: "Smart Students Deserves Smart Classes",
-  whatsappNumber: "9506404083", // Enter WhatsApp number (country code + number, no + or spaces)
+  whatsappNumber: "919506404083", // Enter WhatsApp number (country code + number, no + or spaces)
   whatsappMessage: "Hi Anoop Sir! I have a question about Math By Anoop classes.",
 };
